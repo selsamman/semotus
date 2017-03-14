acdee2d04a359805f3928e6267a8a1b3f3842215
Sam Elsamman <sam.elsamman@gmail.com>
2017-03-14 17:41:08 -0400

    include Remotable and Persistable mixins for benefit of browser

--- index.js
+++ index.js
@@ -2550,6 +2550,40 @@ RemoteObjectTemplate.bindDecorators = function (objectTemplate) {
     }
 }
 
+// These two mixins and extender functions are needed because in the browser we only include supertype
+// and since classes use these in their extends hierarchy they must be defined.
+
+var __extends = (this && this.__extends) || (function () {
+        var extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return function (d, b) {
+            extendStatics(d, b);
+            function __() { this.constructor = d; }
+            d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+        };
+    })();
+
+RemoteObjectTemplate.Persistable = function (Base) {
+    return (function (_super) {
+        __extends(class_1, _super);
+        function class_1() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return class_1;
+    }(Base));
+}
+RemoteObjectTemplate.Remoteable = function (Base) {
+    return (function (_super) {
+        __extends(class_1, _super);
+        function class_1() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return class_1;
+    }(Base));
+}
+
+
 RemoteObjectTemplate.bindDecorators(); //Default to binding to yourself
 
 return RemoteObjectTemplate;
